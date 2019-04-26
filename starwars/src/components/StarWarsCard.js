5b9a645aa25712b48022b438c20c9727a8cc1c91
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-26 10:28:30 -0500

    SC: Starwars: Completed displaying data on page without a listing
    The data that was pulled from the api is displayed on the page. The font used is google font lato.

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./StarWars.css";
 
 function StarWarsCard(props) {
-  return <div class="card">This is in Star Wars card</div>;
+  return (
+    <div class="card">
+      <p className="cardText">Name: {props.character.name}</p>
+      <p className="cardText">Height: {props.character.height}</p>
+      <p className="cardText">Hair color: {props.character.hair_color}</p>
+      <p className="cardText">Birth year: {props.character.birth_year}</p>
+      <p className="cardText">Gender: {props.character.gender}</p>
+    </div>
+  );
 }
 export default StarWarsCard;
