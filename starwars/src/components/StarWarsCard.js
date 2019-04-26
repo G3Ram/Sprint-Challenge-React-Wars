2b4b6e0fe096d97bb080dd1d2bd41857631332b1
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-26 10:42:43 -0500

    SC: Starwars: Recursive printing of movie links
    The movie names are printed recursively as a link. Created a class for Film to get the details of Film from the api link

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./StarWars.css";
+import Film from "./Film";
 
 function StarWarsCard(props) {
   return (
@@ -9,6 +10,10 @@ function StarWarsCard(props) {
       <p className="cardText">Hair color: {props.character.hair_color}</p>
       <p className="cardText">Birth year: {props.character.birth_year}</p>
       <p className="cardText">Gender: {props.character.gender}</p>
+      <p className="cardText"> Movies:</p>
+      {props.character.films.map((film, index) => (
+        <Film key={index} film={film} />
+      ))}
     </div>
   );
 }
